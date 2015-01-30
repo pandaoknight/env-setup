aa84dac902c7642927fe45841d6d1e46371d9af2
PC <pandaoknight@users.noreply.github.com>
2015-01-30 07:45:15 +0000

    +增加Git的color editor alias配置

---
+++ git_config.sh
@@ -0,0 +1,16 @@
+# 1. 默认情况下，ubuntu下的终端中，git没有颜色,可以使用如下命令给git配色
+git config --global color.status auto
+git config --global color.diff auto
+git config --global color.branch auto
+git config --global color.interactive auto
+
+# 2. git默认编辑器设置
+git config --global core.editor vim
+
+# 3. git alias设置
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+#git config --global alias.df diff
+git config --global alias.br branch
+
