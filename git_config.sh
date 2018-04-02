3e616a86506865b31efbf26d646e8c684927d8d4
pandaoknight@ <pandaoknight@.com>
2018-04-02 11:26:36 +0800

    add alias logpretty

--- git_config.sh
+++ git_config.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 # 1. 默认情况下，ubuntu下的终端中，git没有颜色,可以使用如下命令给git配色
 git config --global color.status auto
 git config --global color.diff auto
@@ -17,6 +17,8 @@ git config --global alias.br branch
 #by me
 git config --global alias.df diff
 git config --global alias.ds "diff --staged"
+
+git config --global alias.logpretty "log --pretty=format:'%h - %an, %ar : %s'"
 
 # 4. 设置git的user
 git config --global user.name "pandaoknight@$HOSTNAME"
