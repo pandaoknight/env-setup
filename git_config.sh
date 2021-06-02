fa6496a5ed982d32eaa2c8185f54371c660952e2
PC <pandaoknight@users.noreply.github.com>
2021-06-02 17:55:40 +0800

    +alias.pk

--- git_config.sh
+++ git_config.sh
@@ -13,6 +13,7 @@ git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
+git config --global alias.pk cherry-pick
 
 #by me
 git config --global alias.df diff
