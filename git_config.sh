a813eebf75faf9413be2d2f0059c307cb077158b
PC <pandaoknight@users.noreply.github.com>
2022-12-04 09:30:39 +0800

    add alias for switch and restore

--- git_config.sh
+++ git_config.sh
@@ -11,6 +11,8 @@ git config --global core.editor vim
 # 3. git alias设置
 git config --global alias.st status
 git config --global alias.co checkout
+git config --global alias.sw switch
+git config --global alias.rs restore
 git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.pk cherry-pick
